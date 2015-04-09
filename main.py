d930ade9855db53dc761a3579fcd26d36b4e0073
admin <axellerate@hotmail.com>
2015-04-08 23:43:36 -0400

    added emissions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import random
 import hashlib
@@ -7,15 +8,18 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 import datetime
 import webapp2
 import jinja2
+import urllib2
 from google.appengine.ext import endpoints
 from google.appengine.ext import ndb
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
@@ -83,21 +87,33 @@ class Cars(ndb.Model):
     make = ndb.StringProperty(required = True)
     model = ndb.StringProperty(required = True)
     year = ndb.IntegerProperty(required = True)
+    carType = ndb.StringProperty(required = True)
     created = ndb.DateTimeProperty(auto_now_add = True)
     edited = ndb.DateTimeProperty(auto_now = True)
+    emissionsPerKm = ndb.FloatProperty(required = True)
 
 class UserCars(ndb.Model):
     user = ndb.KeyProperty(kind = Users)
     car = ndb.KeyProperty(kind = Cars)
 
-class UserFuel(ndb.Model):
-    user = ndb.KeyProperty(kind = Users)
-    litres = ndb.FloatProperty(required = True)
-
 class CarEmissions(ndb.Model):
     car = ndb.KeyProperty(kind = Cars)
     grams = ndb.IntegerProperty(required = True)
 
+def create_countries():
+    list_countries = countries.split('},')
+
+    countries2 = Country.query()
+    for i in countries2:
+        i.key.delete()
+
+    for i in list_countries:
+        start = i.find("name: '")
+        end = i.find("', code:")
+        country = i[start+7:end]
+        country_instance = Country(name = country)
+        country_instance.put()
+    return
 
 #API Methods#
 
@@ -117,6 +133,7 @@ class CreateUser(messages.Message):
     firstName = messages.StringField(2, required = True)
     lastName = messages.StringField(3, required = True)
     pw_hash = messages.StringField(4, required = True)
+    country = messages.StringField(5, required = True)
 
 
 class AddCar(messages.Message):
@@ -144,8 +161,10 @@ class UserApi(remote.Service):
                         path = 'createuser',
                         http_method = 'POST')
     def createUser(self, request):
-        Users(email = request.email, firstName = request.firstName,
-              lastName = request.lastName, pw_hash = request.pw_hash).put()
+        user_query = Users.query(Users.email == request.email)
+        if not user_query.get():
+          Users(email = request.email, firstName = request.firstName,
+                lastName = request.lastName, pw_hash = request.pw_hash).put()
         return request
 
     @endpoints.method(AddCar, message_types.VoidMessage,
@@ -173,6 +192,7 @@ class CreateCar(messages.Message):
     model = messages.StringField(2)
     year = messages.IntegerField(3)
     carId = messages.IntegerField(4)
+    carType = messages.StringField(5)
 
 class AllCarsList(messages.Message):
     cars = messages.MessageField(CreateCar, 1, repeated = True)
@@ -186,7 +206,14 @@ class CarsApi(remote.Service):
                         path = 'addcar',
                         http_method = 'POST')
     def createCar(self, request):
-        Cars(make = request.make, model = request.model, year = request.year, carId = int(request.carId)).put()
+        car_query = Cars.query(Cars.carId == int(request.carId))
+        if not car_query.get():
+          url = "http://fueleconomy.gov/ws/rest/vehicle/%s" %request.carId
+          xml = urllib2.urlopen(url).read()
+          start = xml.find("<co2>")+5
+          end = xml.find("</co2>")
+          emissions = float(xml[start:end]) * 0.621371
+          Cars(emissionsPerKm = emissions, make = request.make, model = request.model, year = int(request.year), carId = int(request.carId), carType = request.carType).put()
         return request
 
     @endpoints.method(message_types.VoidMessage, AllCarsList,
@@ -206,15 +233,34 @@ class CarsApi(remote.Service):
                         path = 'getcar',
                         http_method = 'GET')
     def getCar(self, request):
-        car_query = Cars.query(Cars.carId == request.carId)
+        car_query = Cars.query(Cars.carId == int(request.carId))
         for i in car_query:
             car = CreateCar(make = i.make, model = i.model,
                             year = i.year, carId = i.carId)
         return car
 
+class ShowCountries(messages.Message):
+    name = messages.StringField(1)
 
+class AllCountries(messages.Message):
+    countries = messages.MessageField(ShowCountries, 1, repeated = True)
 
-application = endpoints.api_server([UserApi, CarsApi])
+@endpoints.api(name = 'countries', version = 'v1',
+               description = 'Fetch countries')
+class CountriesApi(remote.Service):
+
+    @endpoints.method(message_types.VoidMessage, AllCountries,
+                        name = 'countries.show',
+                        path = 'countries',
+                        http_method = 'GET')
+    def showCountries(self, request):
+        all_countries = []
+        countries = Country.query().order(Country.name)
+        for i in countries:
+            all_countries.append(ShowCountries(name = i.name))
+        return AllCountries(countries = all_countries)
+
+application = endpoints.api_server([UserApi, CarsApi, CountriesApi])
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
